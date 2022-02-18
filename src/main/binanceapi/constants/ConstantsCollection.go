3c1efe15955545098d6962715209331c4024069c
Pauli <pauli_feu@yahoo.de>
2022-02-18 01:48:08 +0100

    added class for constants to store for the whole project

---
+++ src/main/binanceapi/constants/ConstantsCollection.go
@@ -0,0 +1,9 @@
+package constants
+
+const (
+	BINANCE_BASE_ENDPOINT        = "api.binance.com"
+	BINANCE_TEST_NET             = "testnet.binance.vision"
+	BINANCE_MARKET_DATA_ENDPOINT = "binance.com"
+
+	HTTPS = "https"
+)
