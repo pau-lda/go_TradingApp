246fee9cd4c3e0e03c0879dfd687e53c481e75ea
Pauli <pauli_feu@yahoo.de>
2022-02-18 01:44:00 +0100

    added test class to test api calls

--- src/test/api/api_test.go
+++ src/test/api/api_test.go
@@ -1,50 +1,15 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	marketData "go_TradingApp/main/binanceapi/marketDataEndpoints"
-	"io/ioutil"
-	"log"
-	"net/http"
-	"os"
+	"go_TradingApp/main/binanceapi"
 	"testing"
 )
 
 func TestCall(t *testing.T) {
 
-	response, err := http.Get("https://binance.com/api/v3/time")
-	response2, err2 := http.Get("https://binance.com/api/v3/exchangeInfo")
+	var test binanceapi.SubAPICall
 
-	if err != nil {
-		fmt.Print(err.Error())
-		os.Exit(1)
-	}
-	if err2 != nil {
-		fmt.Print(err2.Error())
-		os.Exit(2)
-	}
-
-	responseData, err := ioutil.ReadAll(response.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
-	responseData2, err2 := ioutil.ReadAll(response2.Body)
-	if err2 != nil {
-		log.Fatal(err2)
-	}
-
-	//fmt.Println(string(responseData2))
-
-	var ResponseObject marketData.ServerTime
-	var ResponseObject2 marketData.ExchangeInfo
-
-	json.Unmarshal(responseData, &ResponseObject)
-	json.Unmarshal(responseData2, &ResponseObject2)
-
-	fmt.Println(ResponseObject.GetServerTimeFormatted())
-	_symbols := ResponseObject2.GetSymbols()
-	for i := 0; i < len(_symbols); i++ {
-		fmt.Println(_symbols[i].Symbol)
-	}
+	time := test.GetServerTime()
+	fmt.Println(time.GetServerTime())
 }
